225c7f41e92dff4b238c62dc593dd82cf0a54294
Akash <akashmagar7500@gmail.com>
2025-08-20 22:26:00 +0530

    navbar added

---
+++ components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { navLinks } from "@/utils/const";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between py-2 items-center">
+      <Link href="/">.NextStep</Link>
+      <div className="flex gap-6">
+        {navLinks.map((link, i) => (
+          <Link key={i} href={link.href}>
+            {link.name}
+          </Link>
+        ))}
+      </div>
+      <div className="space-x-3">
+        <Button>Login</Button>
+        <Button>Sign Up</Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ utils/const.js
@@ -0,0 +1,7 @@
+  export const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Jobs", href: "/jobs" },
+    { name: "Companies", href: "/companies" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" },
+  ];
